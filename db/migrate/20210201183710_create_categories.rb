4d3eeec9e25d8ec09f19634c43b306fd5a9c89f4
Ali <morewater1234@mail.ru>
2021-02-04 04:17:37 +0600

    add reference, and delete some models

--- db/migrate/20210201183710_create_categories.rb
+++ db/migrate/20210201183710_create_categories.rb
@@ -1,7 +1,7 @@
 class CreateCategories < ActiveRecord::Migration[6.1]
   def change
     create_table :categories do |t|
-      t.text :title
+      t.text :title, null: false
 
       t.timestamps
     end
--- db/migrate/20210201184902_add_answers_correct_null_constraint.rb
+++
@@ -1,5 +0,0 @@
-class AddAnswersCorrectNullConstraint < ActiveRecord::Migration[6.1]
-  def change
-    change_column_null(:answers, :correct, false)
-  end
-end
