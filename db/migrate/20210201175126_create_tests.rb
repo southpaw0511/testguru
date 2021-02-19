46b76714cc49f179304b09127d23fc7c1307eba9
Ali <morewater1234@mail.ru>
2021-02-19 14:50:33 +0600

    debug 19.02

--- db/migrate/20210201175126_create_tests.rb
+++ db/migrate/20210201175126_create_tests.rb
@@ -2,6 +2,7 @@ class CreateTests < ActiveRecord::Migration[6.1]
   def change
     create_table :tests do |t|
       t.text :body, null: false
+      t.string :author, null: false
       t.integer :level, null: false, default: 0
       t.references :category, null: false, foreign_key: true
 
--- db/migrate/20210201183453_create_questions.rb
+++ db/migrate/20210201183453_create_questions.rb
@@ -2,7 +2,6 @@ class CreateQuestions < ActiveRecord::Migration[6.1]
   def change
     create_table :questions do |t|
       t.text :body, null: false, limit: 70
-      t.string :author, null: false
       t.references :test, null: false, foreign_key: true
 
       t.timestamps
