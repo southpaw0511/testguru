e8d8b7a47d578e879337bf3343894064061ac889
Ali <morewater1234@mail.ru>
2021-02-27 00:45:12 +0600

    27.02 debug, change author to reference

--- db/migrate/20210226063243_create_tests.rb
+++ db/migrate/20210226063243_create_tests.rb
@@ -2,9 +2,9 @@ class CreateTests < ActiveRecord::Migration[6.1]
   def change
     create_table :tests do |t|
       t.text :body, null: false
-      t.string :author, null: false
       t.integer :level, null: false, default: 0
       t.references :category, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: true
 
       t.timestamps
     end
