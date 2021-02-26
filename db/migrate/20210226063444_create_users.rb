9f3858d38b24e7749e026f938cae6b087f469d2b
Ali <morewater1234@mail.ru>
2021-02-26 12:49:05 +0600

    26.02 create migrate

---
+++ db/migrate/20210226063444_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :password, null: false, limit: 10
+
+      t.timestamps
+    end
+  end
+end
