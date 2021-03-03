89a9da37dc22bae1e600c4eb20717b75e4b51eb9
Ali <morewater1234@mail.ru>
2021-03-03 18:17:51 +0600

    02.03 task4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def show_passed_tests_by_level(level)
+    Test.joins("INNER JOIN results ON tests.id = results.test_id").where(results: {user_id: self.id}, tests: {level: level})
+  end
 end
